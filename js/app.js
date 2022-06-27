b26352e78398d1a5e5620c065a3a680372da32b1
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-06-27 21:07:53 +0300

    created first version

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 import { isWebp } from './components/isWebp.js'
 
 isWebp()
+
+const $burgerButton = document.querySelector('.header__burger')
+const $headerMenu = document.querySelector('.header__menu')
+const $headerButtons = document.querySelector('.header__buttons')
+const $headerNav = document.querySelector('.header__nav')
+const $body = document.body
+
+$burgerButton.addEventListener('click', (e) => {
+	$headerNav.classList.contains('active') ? unlockScroll($body) : lockScroll($body)
+	$headerNav.classList.toggle('active')
+	$burgerButton.classList.toggle('active')
+	$headerMenu.classList.toggle('active')
+	$headerButtons.classList.toggle('active')
+})
+
+function lockScroll(element) {
+	const lockPadding = window.innerWidth - document.body.clientWidth + 'px'
+	setTimeout(() => {
+		element.classList.add('lock-scroll')
+		element.style.paddingRight = lockPadding
+	}, 200)
+}
+
+function unlockScroll(element) {
+	setTimeout(() => {
+		element.classList.remove('lock-scroll')
+		element.style.paddingRight = ''
+	}, 200)
+}
